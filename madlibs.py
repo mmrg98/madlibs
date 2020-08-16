e98f405c5503f0d44134fc6856ec0cd2ec5ed900
Mariam <mariam.elgaria98@gmail.com>
2020-08-16 15:07:58 +0300

    calculator is done

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,21 @@
 def main():
-        time=input("choose time from 1-12: ")
-        items=input("enter any items: ")
-        name=input("enter any name: ")
-        scream=input("enter screaming phase: ")
-        action=input("enter any action: ")
-        print(""" It was %s o\'clock when I heard a knock at the door.
-I opened the door and there was a box full of %s with a note saying \"From Mr. %s \"
-Just as I closed the door I heard a scream \"%s.\"
-I froze in place and all I could do was %s.""" % (time, items, name.title(), scream.upper(), action))
-
+        num1=input("Enter first number: ")
+        num2=input("Enter second number: ")
+        op=time=input("Choose the operation(+, -, /, *): ")
+        listOp=["+","-","/","*"]
+        if (num1.isdigit and num2.isdigit()):
+                if(op=="+"):
+                        print("The answer is ", (int(num1)+int(num2)))
+                elif(op=="-"):
+                        print("The answer is ", (int(num1)-int(num2)))
+                elif(op=="*"):
+                        print("The answer is ", (int(num1)*int(num2)))
+                elif(op=="/"):
+                        print("The answer is ", (int(num1)/int(num2)))
+                else:
+                        print("the operation is not valid")
+        else:
+                print("the numbers are invalid")
 
 if __name__ == '__main__':
 	main()
